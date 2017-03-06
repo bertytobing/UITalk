fc5244ddbc293683120e2b2332bfa4a79a4803c9
galihap <galih.a.pradana@gdplabs.id>
2017-03-06 14:17:34 +0700

    Add api gateway

---
+++ api-gateway/src/main/java/demo/uitalk/UitalkApplication.java
@@ -0,0 +1,12 @@
+package demo.uitalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UitalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UitalkApplication.class, args);
+	}
+}
